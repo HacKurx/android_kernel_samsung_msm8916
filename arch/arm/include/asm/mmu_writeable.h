bc2e362e2867387d5f652377e7a4591f0addad79
HacKurx <hackurx@gmail.com>
2020-04-03 12:54:16 +0200

    Revert "mm: add wrapper function for writing word to kernel text space"
    This reverts commit 7b7d17f49ce07dc89a9a4aa5166561600c0a8fca.

--- arch/arm/include/asm/mmu_writeable.h
+++ arch/arm/include/asm/mmu_writeable.h
@@ -26,6 +26,4 @@ static inline void mem_text_address_restore(void) {};
 static inline void mem_text_writeable_spinunlock(unsigned long *flags) {};
 #endif
 
-void mem_text_write_kernel_word(unsigned long *addr, unsigned long word);
-
 #endif
